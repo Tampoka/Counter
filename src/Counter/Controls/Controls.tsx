08b092de4b9b481e2f92d8c91885564bb7ebdb4b
Tampoka <goal.thanks@gmail.com>
2021-09-28 16:28:44 +0900

    created 3 components for Counter

---
+++ src/Counter/Controls/Controls.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Controls=()=>{
+    return <div>
+        <button>Inc</button>
+        <button>Reset</button>
+    </div>
+}
---
+++ src/Counter/Counter.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Controls} from "./Controls/Controls";
+import {Display} from "./Display/Display";
+
+export const Counter=()=>{
+    return <div>
+        <Display/>
+        <Controls/>
+    </div>
+}
