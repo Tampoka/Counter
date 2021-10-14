0e002f068533985fa62ad0826f7267d83f435893
Tampoka <goal.thanks@gmail.com>
2021-10-14 16:20:14 +0900

    added class error

--- src/Counter/Display/Display.tsx
+++ src/Counter/Display/Display.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import s from "./Display.module.css"
 
 export type DisplayPropsType={
     count:number
@@ -6,6 +7,6 @@ export type DisplayPropsType={
 }
 export const Display=(props:DisplayPropsType)=>{
     return <div>
-        <div className={props.count===props.maxValue?"display":"maxValue"}>{props.count}</div>
+        <div className={props.count===props.maxValue?s.error:""}>{props.count}</div>
     </div>
 }
