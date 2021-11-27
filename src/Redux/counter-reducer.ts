9599558a8f7bb055ef7e6ed7387a8fa4202ffd83
Tampoka <goal.thanks@gmail.com>
2021-11-27 09:17:38 +0900

    Modify initial state + AC

--- src/Redux/counter-reducer.ts
+++ src/Redux/counter-reducer.ts
@@ -1,61 +1,53 @@
-export type CounterValuesType = {
-    maxValue: number
-    minValue: number
-    currentValue?:number
-}
-
 export type StateType = {
-    counterValues: CounterValuesType,
-    newCounterValues: CounterValuesType
+    maxValue: number,
+    minValue: number,
+    currentValue: number
 }
 
-const SET_NEW_MAX_VALUE = "SET_NEW_MAX_VALUE";
-const SET_NEW_MIN_VALUE = "SET_NEW_MIN_VALUE";
-const SET_COUNTER_VALUES = "SET_COUNTER_VALUES";
+const SET_MAX_VALUE = "SET_MAX_VALUE";
+const SET_MIN_VALUE = "SET_MIN_VALUE";
+const SET_CURRENT_VALUE = "SET_CURRENT_VALUE";
+
 
 let initialState = {
-    counterValues: {
-        maxValue: 5,
-        minValue: 0,
-        currentValue:0
-    },
-    newCounterValues: {
-        maxValue: 5,
-        minValue: 0
-    }
+    maxValue: 5,
+    minValue: 0,
+    currentValue: 0
 }
 
 export const counterReducer = (state: StateType = initialState, action: ActionValuesType): StateType => {
     switch (action.type) {
-        case SET_NEW_MAX_VALUE:
-            return {...state, newCounterValues: {...state.newCounterValues, maxValue: action.value}};
-        case SET_NEW_MIN_VALUE:
-            return {...state, newCounterValues: {...state.newCounterValues, minValue: action.value}};
-        case SET_COUNTER_VALUES:
-            return {...state, counterValues: {maxValue: action.maxValue, minValue: action.minValue}};
+        case SET_MAX_VALUE:
+            return {...state, maxValue: action.value};
+        case SET_MIN_VALUE:
+            return {...state, minValue: action.value};
+        case SET_CURRENT_VALUE:
+            return {...state, currentValue: action.value};
         default:
             return state;
     }
 }
 
-type SetNewMaxValueACType = ReturnType<typeof setNewMaxValueAC>
-type SetNewMinValueACType = ReturnType<typeof setNewMinValueAC>
-type SetCounterValuesACType = ReturnType<typeof setCurrentValueAC>
+type SetMaxValueACType = ReturnType<typeof setMaxValueAC>
+type SetMinValueACType = ReturnType<typeof setMinValueAC>
+type setCurrentValueACType = ReturnType<typeof setCurrentValueAC>
 
-export type ActionValuesType = SetNewMaxValueACType | SetNewMinValueACType | SetCounterValuesACType
+export type ActionValuesType =
+    SetMaxValueACType
+    | SetMinValueACType
+    | setCurrentValueACType
 
-export const setNewMaxValueAC = (value: number) => ({
-    type: SET_NEW_MAX_VALUE,
+export const setMaxValueAC = (value: number) => ({
+    type: SET_MAX_VALUE,
     value
 } as const);
 
-export const setNewMinValueAC = (value: number) => ({
-    type: SET_NEW_MIN_VALUE,
+export const setMinValueAC = (value: number) => ({
+    type: SET_MIN_VALUE,
     value
 } as const);
 
-export const setCurrentValueAC = (maxValue: number, minValue: number) => ({
-    type: SET_COUNTER_VALUES,
-    maxValue,
-    minValue
+export const setCurrentValueAC = (value: number) => ({
+    type: SET_CURRENT_VALUE,
+    value
 } as const);
